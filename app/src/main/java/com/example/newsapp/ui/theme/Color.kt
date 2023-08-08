8f49e8757407bacf836a5163d690bbdd42e8b066
SEKI-YUA <zard.since1991@gmail.com>
2023-08-08 20:11:57 +0900

    一部の色と変数名を変更

--- app/src/main/java/com/example/newsapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/newsapp/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.example.newsapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+// ダークテーマで使う色
+val Purple80 = Color(0xFF4190a5)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
+// ライトテーマで使う色
+val zardBlue_light = Color(0xFF57c3e0)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val red_light = Color(0xFFF28F77)
